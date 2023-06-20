18c970f062eeb8af7ad400e270c4ef9322f8dce6
Fisher__SHADOW <hadi.h9589@gmail.com>
2023-06-20 16:05:21 +0330

    submit function/ styles added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,10 @@
-// let rating=0
-// let siblings= document.getElementsByClassName('rate')
-        
-// function selected1() {
-//     rating=1
-//     let selection1=document.getElementById('rate1')
-//     selection1.classList.toggle('selected')          
-//     console.log(rating)
-// }
-// let selected2 = function(){
-//     let selection2=document.getElementById('rate2')
-//     selection2.classList.toggle('selected')       
-// }
+let front=document.getElementById('cardsFront')
+let back=document.getElementById('cardsBack')
+let rateResult=document.getElementById('rateResult')
 let frontSide= document.getElementById('front-side')
 let backSide= document.getElementById('back-side')
-let selectedId= null
-console.log(buttons)
+let selectedId= 0
+
 function resetSelection(){
     const ratings = document.querySelectorAll('.rate');
     ratings.forEach((rating) => {
@@ -22,33 +12,43 @@ function resetSelection(){
     });
 }
 function selected1() {
-    selectedId='rate1'
+    selectedId='1'
     let selection1 = document.getElementById('rate1')
     resetSelection()
     selection1.classList.toggle('selected')
 }
 function selected2() {
-    selectedId='rate2'
+    selectedId='2'
     let selection2 = document.getElementById('rate2')
     resetSelection()
     selection2.classList.toggle('selected')
-    
 }
 function selected3() {
-    selectedId='rate3'
+    selectedId='3'
     let selection3 = document.getElementById('rate3')
     resetSelection()
     selection3.classList.toggle('selected') 
 }
 function selected4() {
-    selectedId='rate4'
+    selectedId='4'
     let selection4 = document.getElementById('rate4')
     resetSelection()
     selection4.classList.toggle('selected') 
 }
 function selected5() {
-    selectedId='rate5'
+    selectedId='5'
     let selection5 = document.getElementById('rate5')
     resetSelection()
     selection5.classList.toggle('selected') 
+}
+function submit() {
+    if (selectedId==0) {
+        alert('please select an score')
+        
+    }else{front.style.display='none'
+    back.style.display='block'
+    rateResult.innerText='You selected '+selectedId+' out of 5'}
+    
+    
+
 }
